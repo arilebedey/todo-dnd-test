af1beadab6e6d64fa3be24280cde8145fbccf99f
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-09-02 19:58:34 +0200

    Extended tailwind config with the design colors and applied background color to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        general_color: "#9E78CF",
+        background: "#0D0714",
+        white: "#FFFFFF",
+        form_border: "#3E1671",
+        completed_todo: "#6BB79D",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
