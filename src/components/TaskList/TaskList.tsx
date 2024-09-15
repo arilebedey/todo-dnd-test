e19900434bcf3f2710c993a6aa5c27cd7daf7f62
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-09-15 13:41:35 +0200

    TaskList now renders removeTask and toggleTask (completed task) changes + tasksCount fix

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,31 +1,56 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { PiTrashSimple } from "react-icons/pi";
-import { RiCheckboxBlankCircleLine } from "react-icons/ri";
+import {
+  RiCheckboxBlankCircleFill,
+  RiCheckboxBlankCircleLine,
+} from "react-icons/ri";
 import { FiEdit3 } from "react-icons/fi";
+import { removeTask, toggleTask } from "./TasksSlice";
 
 export const TaskList = () => {
   const tasks = useSelector((state: RootState) => state.task || []);
+  const dispatch = useDispatch();
 
   if (tasks.length === 0) {
     return null;
   }
 
+  const handleRemoveTask = (id: number) => {
+    dispatch(removeTask(id));
+  };
+
+  const handleToggleTask = (id: number) => {
+    dispatch(toggleTask(id));
+  };
+
   return (
     <div className="w-full">
       <ul className="flex flex-col w-full">
         {tasks.map((task) => (
-          <div className="flex w-full min-h-[80px] items-center justify-between rounded-lg bg-theme-800 my-2">
+          <div
+            className="flex w-full min-h-[80px] items-center justify-between rounded-lg bg-theme-800 my-2"
+            key={task.id}
+          >
             <li
-              className="text-lg text-theme-100 text-left ml-4 pr-4 py-3"
-              key={task.id}
+              className={`text-lg text-left ml-4 pr-4 py-3 ${task.completed ? "line-through text-green" : "text-theme-100"}`}
             >
               {task.title}
             </li>
             <div className="flex">
-              <RiCheckboxBlankCircleLine className="size-5 text-theme-100 mr-4" />
-              <FiEdit3 className="size-5 text-theme-100 mr-4" />
-              <PiTrashSimple className="size-5 text-theme-100 mr-4" />
+              <button onClick={() => handleToggleTask(task.id)}>
+                {!task.completed ? (
+                  <RiCheckboxBlankCircleLine className="size-5 text-theme-100 mr-4" />
+                ) : (
+                  <RiCheckboxBlankCircleFill className="size-5 text-theme-100 mr-4" />
+                )}
+              </button>
+              <button>
+                <FiEdit3 className="size-5 text-theme-100 mr-4" />
+              </button>
+              <button onClick={() => handleRemoveTask(task.id)}>
+                <PiTrashSimple className="size-5 text-theme-100 mr-4 cursor-pointer" />
+              </button>
             </div>
           </div>
         ))}
--- src/components/TasksCount/TasksCount.tsx
+++ src/components/TasksCount/TasksCount.tsx
@@ -2,7 +2,10 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 
 export const TasksCount = () => {
-  const tasksLength = useSelector((state: RootState) => state.task.length);
+  const tasksLength = useSelector((state: RootState) => {
+    const notComplete = state.task.filter((task) => task.completed !== true);
+    return notComplete.length;
+  });
 
   return (
     <div className="flex items-center w-full pt-14">
